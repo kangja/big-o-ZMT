e4964c87fb748a051c52ebc61fbfd3ced7b2baf7
hi <kangja1996@gmail.com>
2020-10-27 09:42:00 -0400

    progress

---
+++ notes2.js
@@ -0,0 +1,10 @@
+function findNemo(array) {
+  let t0 = performance.now()
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === "nemo") {
+      console.log("Found NEMO!")
+    }
+  }
+  let t1 = performance.now();
+}
+//lets measure the performance of this function. Javascript has a tool that comes built-in to the browser. We will use this example to measure how long it takes for this functino to run. We can do this by doing time0 is going to equal performance that comes built in to the browser and we just say now. And t0(timer0) will start the timer before the loop happens. And when the loop ends, I'm going to have anther timer called t1 and this is going to equal performance.now. So we have two timers, one at the beginning and one at the end after the function goes through the loop to find Nemo. 
