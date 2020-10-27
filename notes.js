e112308e2202be943fc5b202e31f28b394cc400e
hi <kangja1996@gmail.com>
2020-10-26 23:58:15 -0400

    progress

--- notes.js
+++ notes.js
@@ -1,4 +1,13 @@
 //BIG O
+
 //The official term is Big O asymtomatic analysis.
+
 //Any coder when given enough time can solve a problem. But, what matter is how well the problem is solved and this is where Big-O can help us. It can tell us how well a problem is solved. 
+
 //We use Big O and its different notation to distinguish the bad code from good code and good code from great code. 
+
+//What is a good code?
+//There are two main things we talk about when we say this is good code.
+//1. Readable: is your code generally clean? Can others understand your code?
+//2. Scalable: 
+ 
