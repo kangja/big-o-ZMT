0cdf621325e416e4068beb116a5f47994144986f
hi <kangja1996@gmail.com>
2020-10-30 23:56:03 -0400

    progress

--- notes3.js
+++ notes3.js
@@ -6,6 +6,15 @@
 
 //We say that finding Nemo function has a Big O notation of O(n). We say Big O of N or what we call linear time. It takes linear time to find a Nemo. Where does this n from O(n) come from? This "n" can be anything. We could put x, y, fish, and etc. It's just an arbitrary letter. We usually give n when it comes to Big-O. This is just a standard that you'll see across the board and it simply means the Big O depends on the number of inputs, number of x, number of fish. 
 
+
+//If we just had the "Nemo" array this would just be O(1).
+
+//If we had the "everyone" array, this would be O(10). 
+
+//If we had the "large" array, it would be O(100,000).
+
+//But as the inputs increase, you see that the number of operations increase linearly with it. O(n) is probably the most common Big O notation you'll find. If you look O(n) in the graph, you see that it's a straight line. The number of operations increases by the same amount because keep this in mind, Big-O doesn't measure things in seconds. Instead, we're focusing on how quickly our runtime grows. We simply do this by using the size of the inputt, which we call n or anything else that we want really. And compare to the number of operations that increase. That's what scalability means. As things grow larger and larger, does it scale? Agin, the find "Nemo" function is O(n) and linear time. 
+
 const nemo = ["nemo"];
 const everyone = ["dory", "bruce", "marlin", "nemo", "gill", "bloat", "nigel", "squirt", "darla", "hank"];
 const large = new Array(100000).fill("nemo");
