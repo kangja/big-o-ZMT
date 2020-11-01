8bd08896a5ce1e69f1f6f45d991a297f7696b7f7
hi <kangja1996@gmail.com>
2020-11-01 15:45:26 -0500

    progress

--- notes5.js
+++ notes5.js
@@ -4,8 +4,11 @@ function funChallenge(input) {
   a = 50 + 3; //O(1) 
 
   for (let i = 0; i < input.length; i++) { //O(n) becuase it just reflects how big the input is. That's how long this "for loops" is going to take. 
+    
     anotherFunction(); //anotherFunction that we don't know what function it is, but it's calling to the function outside of the funChallenge function. This functino runs how many times? Well, it depends on how big our input is. It's also O(n). 
+
     let stranger = true; //another random assignment here. But, this runs as many times as this loop happens. If input was 5, it would run 5 times. Again, this would be O(n). 
+    
     a++; //we're just incrementing the whatever the number is at the time. So 53 will become 54 and then as we loop through, it will keep increasing by 1. 54...55...56...etc. This will again run O(n) times. 
   }
   return a; //this run just once. Everytime funhallenge function is run, the return statement only runs once. O(1).
