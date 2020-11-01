fca1b80293120a6abfb4938fedd24c6c7d3677bb
hi <kangja1996@gmail.com>
2020-11-01 16:04:34 -0500

    notes6 finished

--- notes6.js
+++ notes6.js
@@ -5,17 +5,17 @@ function anotherFunChallenge(input) {
   let c = 50; //O(1)
   //it's all O(1) because we are assigning variables
 
-  for (let i = 0; i < input; i++) {
+  for (let i = 0; i < input; i++) { //we have a for loop that is running based on the input. We don't know what the input is, but we can just assume that we're looping based on however long this input is. In which case, we're running each one of these O(n) times. 
     let x = i + 1; //O(n)
     let y = i + 2; //O(n)
     let z = i + 3; //O(n)
 
   }
-  for (let j = 0; j < input; j++) {
+  for (let j = 0; j < input; j++) { //we can include this as O(n) as well if we would like. We don't know what the input is, but but there are being run O(n) times. 
     let p = j * 2; //O(n)
     let q = j * 2; //O(n)
   }
   let whoAmI = "I don't know"; //O(1)
 } 
 
-//4+3(n)
+//Our BIG O is O(4 + 5n). We have 4 constant times and then 5 things that we are doing n times! Let's start to figure out how we can simplify things for us so that we're not doing this mathematical step by step count. 
