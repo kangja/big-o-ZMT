c7cba10a4fed72a1575404ce203cce645641920b
hi <kangja1996@gmail.com>
2020-11-03 18:00:03 -0500

    progress

--- notes9-big-o-rule-2.js
+++ notes9-big-o-rule-2.js
@@ -1,2 +1,19 @@
 // RULE NUMBER 2
 // Remove constants
+
+function printFirstItemThenFirstHalfThenSayHi100Times(items) {
+  console.log(items[0]); //O(1)
+
+  let middleIndex = Math.floor(items.length / 2); //we have half of whatever the items is. In this case, it will be O(n/2). Because even though we have a loop or a while loop, we're only logging half the items every time. 
+  
+  let index = 0; 
+
+  while (index < middleIndex) {
+    console.log(items[index]);
+    index++;
+  }
+
+  for (let i = 0; i < 100; i++) {
+    console.log("hi");
+  }
+}
