5a2cd48aad4872dd2d457f71fc150872a85605af
hi <kangja1996@gmail.com>
2020-11-04 21:05:25 -0500

    finished

--- notes9-big-o-rule-2.js
+++ notes9-big-o-rule-2.js
@@ -26,3 +26,21 @@ function printFirstItemThenFirstHalfThenSayHi100Times(items) {
 // Because 1 from O(n+1) is insignificant if n was a million, we can just drop it as well. This function just becomes O(n).  
 
 // What if we had another function that had O(a + 50) or O(a + 50000000000)? It doesn't really matter. We drop the constants. It's gonna be O(a). 
+
+
+
+// Here's another situation!
+// This function has 2 for loops. 
+function compressBoxesTwice(boxes) {
+  boxes.forEach(function(boxes) {
+    console.log(boxes); //O(n)
+  });
+
+  boxes.forEach(function(boxes) {
+    console.log(boxes) //O(n)
+  });
+}
+
+//When we add these two steps, we get O(2n). Again, in an interview, this doesn't really matter because we DROP the ocnstants. If we drop the constant, it just becomes O(n). If we look at the function on the graph, we see that as the elements increase, we have two operations because we have 2 for loops. If we have 2 elements, we have 4 operations. If we have 3 elements, we have 6 operations. The way the line increases is linear and it's the key here. With the Big O, we don't really care about how steep the line is. We care about HOW the line moves as our inputs increase.  
+
+// DROP the constants and you'll never going to see numbers in Big O notation. In fact, the only time you'll see numbers is when it's O(1), O(n^2), or O(2^n). 
